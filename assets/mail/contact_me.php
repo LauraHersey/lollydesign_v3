f4942a45c203df7eadbcc963e7bdfc50fb975116
David Hersey <david.hersey@holidayextras.com>
2020-09-07 12:22:44 +0100

    Hmmm

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,14 +1,14 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_REQUEST['name']) || empty($_REQUEST['email']) || empty($_REQUEST['phone']) || empty($_REQUEST['message']) || !filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$name = strip_tags(htmlspecialchars($_REQUEST['name']));
+$email = strip_tags(htmlspecialchars($_REQUEST['email']));
+$phone = strip_tags(htmlspecialchars($_REQUEST['phone']));
+$message = strip_tags(htmlspecialchars($_REQUEST['message']));
 
 // Create the email and send the message
 $to = "laura@lollydesign.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
